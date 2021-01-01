d639f22faf666b20b968014c7115b97d920854e7
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2021-01-01 21:42:41 +0800

    change date

--- bot.py
+++ bot.py
@@ -11,13 +11,13 @@ async def on_ready():
         channel =  get(guild.text_channels,name='acah-study📚')
         if channel is not None:
             date_now = dt.datetime.now()
-            date_end = dt.datetime(year=2021,month=1,day=23)
+            date_end = dt.datetime(year=2021,month=1,day=24)
             days_left = (date_end - date_now).days
             display_text = f'Hamba hanya nak mengingatkan bahawa sem ni tinggal **{days_left} hari** lagi.'
             if days_left <= 10:
                 display_text += ' Rasa nak terkencing tunggu.'
             elif days_left <= 0:
-                display_text += ' Aikk? habis dah'
+                display_text = 'Aikk? habis dah'
                 
             message = await channel.send(display_text)
             guild_emoji = discord.utils.get(guild.emojis, name='ultraman')   
